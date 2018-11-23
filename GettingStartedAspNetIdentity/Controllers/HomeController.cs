9f521e0eaff806bbf2a99daf2a45365a3bbcf842
robertogamez <vn00m8demail.wal/mart.com>
2018-11-23 14:35:21 -0600

    Cambios para permitir la autenticacion

--- GettingStartedAspNetIdentity/Controllers/HomeController.cs
+++ GettingStartedAspNetIdentity/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,6 +11,7 @@ namespace GettingStartedAspNetIdentity.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public IActionResult Index() => View(new Dictionary<string, object>
         {
              ["Placeholder"] = "Placeholder" 
