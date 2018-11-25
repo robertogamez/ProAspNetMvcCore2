17442d460fb2c0096a136c89ce373bf71833ef9e
robertogamez <vn00m8demail.wal/mart.com>
2018-11-25 00:21:39 -0600

    Iniciando con proceso de login

---
+++ GettingStartedAspNetIdentity/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GettingStartedAspNetIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GettingStartedAspNetIdentity.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(
+            UserManager<AppUser> userMgr,
+            SignInManager<AppUser> signinMgr
+        )
+        {
+            userManager = userMgr;
+            signInManager = signinMgr;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel details, string returnUrl)
+        {
+
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+
+                if(user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(
+                        user, details.Password, false, false
+                    );
+
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+
+                ModelState.AddModelError(nameof(LoginModel.Email), "Invalid user or password");
+            }
+
+            return View(details);
+        }
+    }
+}
--- GettingStartedAspNetIdentity/Models/UserViewModels.cs
+++ GettingStartedAspNetIdentity/Models/UserViewModels.cs
@@ -12,9 +12,20 @@ namespace GettingStartedAspNetIdentity.Models
         public string Name { get; set; }
 
         [Required]
-        public string Email { get; set; } 
+        public string Email { get; set; }
 
         [Required]
+        public string Password { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [UIHint("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [UIHint("password")]
         public string Password { get; set; }
     }
 }
