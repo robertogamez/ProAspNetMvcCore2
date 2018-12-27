ca535a74f3414eeebdf271f22ef7705b652c021b
robertogamez <roberto.mb.gamez@hotmail.com>
2018-12-27 12:14:03 -0600

    Iniciando con proyecto de DI

---
+++ DependencyInjection.Tests/DITests.cs
@@ -0,0 +1,35 @@
+﻿using DependencyInjection.Controllers;
+using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DependencyInjection.Tests
+{
+    public class DITests
+    {
+        [Fact]
+        public void ControllerTest()
+        {
+            // Arrange 
+            var data = new[] { new Product { Name = "Test", Price = 100 } };
+            var mock = new Mock<IRepository>();
+            mock.Setup(m => m.Products).Returns(data);
+            HomeController controller = new HomeController
+            {
+                Repository = mock.Object
+            };
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.Equal(data, result.ViewData.Model);
+        }
+    }
+}
+
+
---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IRepository Repository { get; set; } = new MemoryRepository();
+
+        public IActionResult Index()
+        {
+            return View(Repository.Products);
+        }
+    }
+}
